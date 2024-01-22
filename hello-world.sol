82d6f50bc83acd770968b37705784acbe2cb5459
ChanGe <1021780227@qq.com>
2024-01-22 11:15:09 +0800

    add hello-world,sol

--- hello-world.sol
+++ hello-world.sol
@@ -1,1 +1,11 @@
-pragma solidity ^0.5.4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract Helloworld{
+    uint64 number = 64;
+
+    function addNumber( uint64 operator)  public  pure  returns(uint64 result) {
+        result += operator;
+        return result;
+    }
+}
